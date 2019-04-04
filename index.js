8ec6817ada547ce8021976bb7313c264fab00437
zhifeng ou <zhifengou@hotmail.com>
2019-04-04 20:36:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function findMatching(drivers, string) {
-  return drivers.filter(function(val){return val.toLowerCase()===string.toLowerCase()})
+  return drivers.filter(function(val){
+    return val.toLowerCase()===string.toLowerCase()})
 }
  function fuzzyMatch(drivers, string) {
-  return drivers.filter(function(val){return val.slice(0,string.length)===string})
+  return drivers.filter(function(val){
+    return val.slice(0,string.length)===string})
 }
  function matchName(drivers, string) {
-  return drivers.filter(function(val){return val.name===string})
+  return drivers.filter(function(val){
+    return val.name===string})
 }
