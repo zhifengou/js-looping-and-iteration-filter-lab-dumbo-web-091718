b43e9edccd0ba9e7d2d43c0414d4fe5b0c340669
zhifeng ou <zhifengou@hotmail.com>
2019-04-04 20:36:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function(val){return val.toLowerCase()===string.toLowerCase()})
+}
+ function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(val){return val.slice(0,string.length)===string})
+}
+ function matchName(drivers, string) {
+  return drivers.filter(function(val){return val.name===string})
+}
